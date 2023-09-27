951f61c8dfa3fbdf8e40dce82871e656327cb9e8
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-09-27 22:31:49 +0700

    check is conf dir is exists

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/irdaislakhuafa/octacat-app-backend/src/helper/files"
+)
+
+const (
+	configPath = "./etc/cfg"
+	configFile = "conf.json"
+)
+
+func main() {
+	if !files.IsFileExist(configPath) {
+		panic(fmt.Sprintf("dir %v doesn't exist!", configPath))
+	}
 }
